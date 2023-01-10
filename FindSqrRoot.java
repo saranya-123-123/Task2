268be704b743e8c62781a6cbb8bc92b5658c81fb
saranya-123-123 <122192995+saranya-123-123@users.noreply.github.com>
2023-01-10 17:32:43 +0530

    Add files via upload

---
+++ FindSqrRoot.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class FindSqrRoot {
+
+	public static void main(String[] args) {
+		int num=4;
+		
+		//double sqrt = Math.sqrt(num);
+		double sqrt = Math.sqrt(num);
+		System.out.println(Math.sqrt(num));
+
+	}
+
+}
---
+++ SquareRoot.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class SquareRoot {
+
+	
+	public static void main(String[] args) {
+	int num=8;
+	double sqrt = Math.sqrt(num);
+	
+double floor = Math.floor(sqrt);
+		System.out.println(Math.floor(sqrt));
+	
+
+	
+
+	}
+
+	
+	}
+
